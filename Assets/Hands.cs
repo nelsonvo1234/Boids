b7991194e0c905795d724ae1d651dc1bf8fc2dc1
Nelson Vo <nelsonvo2006@gmail.com>
2025-06-01 14:24:03 -0500

    Hand tracking in Unity

--- Assets/Hands.cs
+++ Assets/Hands.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System;
+using UnityEditor;
 
 public class Hands : MonoBehaviour
 {
@@ -11,10 +13,11 @@ public class Hands : MonoBehaviour
     Socket listener;
     byte[] buffer;
     // Start is called before the first frame update
+    Vector3[] landmarks;
+    string[] sp = { " ", "}" };
     void Start()
-
-    
     {
+        landmarks = new Vector3[21];
         buffer = new byte[2048];
         try
         {
@@ -36,18 +39,40 @@ public class Hands : MonoBehaviour
         }
 
 
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        String received;
         if (sender.Available > 0)
         {
             sender.Receive(buffer);
-            print(Encoding.ASCII.GetString(buffer));
+            received = Encoding.ASCII.GetString(buffer);
+            string[] words = received.Split('{', StringSplitOptions.RemoveEmptyEntries);
+            
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains('}'))
+                {
+                    print(words[i]);
+                    string[] vals = words[i].Split(sp, StringSplitOptions.RemoveEmptyEntries);
+                    landmarks[int.Parse(vals[0])] = new Vector3(float.Parse(vals[1]), float.Parse(vals[2]), float.Parse(vals[3])) * 10; 
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
-        
-        
+    }
+
+    void OnDrawGizmos()
+    {
+        foreach (Vector3 landmark in landmarks)
+        {
+            Gizmos.DrawSphere(landmark, 0.1f);
+        }
     }
 }
